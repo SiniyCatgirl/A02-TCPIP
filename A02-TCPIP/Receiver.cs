d165457e3c1dc923434c413fb08cce3125bc62d7
Josh Visentin <Jvisentin1703@conestogac.on.ca>
2026-02-14 15:15:31 -0500

    Commented receiver

--- A02-TCPIP/Receiver.cs
+++ A02-TCPIP/Receiver.cs
@@ -3,7 +3,8 @@
 *	PROJECT         :   A02 - TCP/IP
 *   PROGRAMMER      :   Jonathan Paventi, Joshua Visentin, Trent Beitz
 *   FIRST VERSION   :   February 10, 20206
-*   DESCRIPTION     :   
+*   DESCRIPTION     :   Handles incoming client message over TCP, interprets protocol commands based on custom defines,
+*                   :   manages game satate and responds to client with appropriate information based on the message received.
 */
 
 using System.Configuration;
@@ -16,7 +17,8 @@ namespace A02_TCPIP {
 
         /*
         Method        : Worker
-        Description   : this listens for the client to send it messages and direcs what to do wih them
+        Description   : Listens for client messages, processes them according to the defined protocol, 
+                      : manages game state, and sends appropriate responses back to the client.
         Parameters    : TcpClient client        :
                         CancellationToken ct    :   The token required for the tasks to know
                                                     when and if the cancellation token has been
@@ -26,48 +28,65 @@ namespace A02_TCPIP {
                                                     to its caller.
         */
         internal async Task Worker(TcpClient client, CancellationToken ct){
-            // read and parse buffer size from appconfig
+            // Get buffer size from config.
             string buffer = ConfigurationManager.AppSettings["BufferSize"];
             int.TryParse(buffer, out int bufferSize);
 
+            // Allocate buffer for incoming client messages.
             Byte[] clientBytes = new byte[bufferSize];
             NetworkStream stream = client.GetStream();
 
+            // Read incoming messages from the client & convert received bytes into ASCII string.
             int i = await stream.ReadAsync(clientBytes, 0, clientBytes.Length, ct);
             string msg = Encoding.ASCII.GetString(clientBytes, 0, i);
 
+            // Get the directory path for game files from config.
             string dirPath = ConfigurationManager.AppSettings["FileList"];
 
+            // Initialize response string and buffer for outgoing messages to client.
             string response = string.Empty;
             Byte[] serverBytes = new byte[bufferSize];
 
+            // Log received messages.
             Console.WriteLine($"Received: {msg}");
             Logger.LogMessage($"Received {msg}");
 
-            switch (msg) {
-                case string s when s.StartsWith(Defines.ID_PREFIX): //initial contact.
+            // Determine the type of message received and process accordingly based on the defined protocol.
+            switch(msg) {
+                // Initial contact from client.
+                case string s when s.StartsWith(Defines.ID_PREFIX):
+                    // Create new game instance for the client.
                     GameState game = new GameState(dirPath);
-                    Logger.LogMessage($"Opened {game.CurrentGameFile}");
+                    
+                    // Log the game file that was opened for the client.
+                    Logger.LogMessage($"Opened {dirPath}{game.CurrentGameFile}");
                     Console.WriteLine($"Found the files: {dirPath}{game.CurrentGameFile}");
 
+                    // Add the new game instance to the clients dictionary with the GameID as the key.
                     Program.clients.Add(game.GameID, game);
                     Console.WriteLine("Added the client to the dictionary!");
 
+                    // create response string with client's unique GameID, clue, & number of words to guess.
                     response = Defines.ID_PREFIX + game.GameID.ToString() + game.GetClue + game.GetWordsToGuess;
                     Console.WriteLine($"Finished: {response}");
 
                     break;
-                case string s when s.StartsWith(Defines.GUESS_PREFIX): // guesses from client
+                // Handle guess from client.
+                case string s when s.StartsWith(Defines.GUESS_PREFIX):
+                    // Extract game ID from the message and retrieve the corresponding game instance from the clients dictionary.
                     Guid clientGameID = Guid.Empty;
                     Guid.TryParse(s.Substring(Defines.GUESS_PREFIX.Length, Defines.GUID_SIZE), out clientGameID);
                     Program.clients.TryGetValue(clientGameID, out GameState clientGame);
 
+                    // skips the prefix of guess and the ID and grabs the string containing the actual guess
                     string guess = msg.Substring(Defines.GUESS_PREFIX.Length + clientGameID.ToString().Length).Trim();
                     guess = guess.ToLower();
-                    bool isRepeat = clientGame.Guesses.Contains(guess);
 
+                    // Check if guess has already been guessed and add to the list if its a unique guess.
+                    bool isRepeat = clientGame.Guesses.Contains(guess);
                     if (!isRepeat) clientGame.AddGuess(guess);
 
+                    // depending on the "guessState" adds one of three prefixes
                     string guessState = null;
                     if (isRepeat){
                         guessState = Defines.GUESS_REPEAT_PREFIX;
@@ -77,40 +96,51 @@ namespace A02_TCPIP {
                         guessState = Defines.GUESS_INCORRECT_PREFIX;
                     }
 
-                    response = guessState + guess;
+                    response = guessState + guess;      // builds response to Client
 
                     break;
-                case string s when s.StartsWith(Defines.GAME_OVER_PREFIX): //Could be TIMEOUT, NEWGAME, ENDGAME. win or lose.
+                // Handle game over messages from client.
+                case string s when s.StartsWith(Defines.GAME_OVER_PREFIX):
                     response = msg;
-
                     Guid gameIDToEnd = Guid.Empty;
                     GameState gameEnded = null;
 
+                    // controls what the server does with the GAME_OVER_PREFIX
                     switch (msg) {
+                        // parse the guid alert the client that they have run out of time, waits for response
                         case string t when t.StartsWith(Defines.GAME_OVER_TIMEOUT_PREFIX):
-                            Guid.TryParse(s.Substring(Defines.GAME_OVER_TIMEOUT_PREFIX.Length), out gameIDToEnd);
+                            //Parse id and get game info from dictionary.
+                            Guid.TryParse(s.Substring(Defines.GAME_OVER_TIMEOUT_PREFIX.Length), out gameIDToEnd);   
                             Program.clients.TryGetValue(gameIDToEnd, out gameEnded);
                             Logger.LogMessage($"Game ended due to TIMEOUT: {gameEnded.CurrentGameFile}, on Client ID: {gameIDToEnd}");
 
                             break;
+                        // gets the userID, logs the new game request, responds with appropriate prefix to Client
                         case string t when t.StartsWith(Defines.GAME_OVER_NEWGAME_PREFIX):
+                            //Parse id and get game info from dictionary.
                             Guid.TryParse(s.Substring(Defines.GAME_OVER_NEWGAME_PREFIX.Length), out gameIDToEnd);
                             Program.clients.TryGetValue(gameIDToEnd, out gameEnded);
                             Logger.LogMessage($"New game requested: {gameEnded.CurrentGameFile}, on Client ID: {gameIDToEnd}");
-                            gameEnded.NewGame();
+                            gameEnded.NewGame();    // erases current game statistics (not ID or games played)
                             response = Defines.GAME_OVER_NEWGAME_PREFIX + gameEnded.GetClue + gameEnded.GetWordsToGuess;
 
                             break;
+                        // Handle end game case.
                         case string t when t.StartsWith(Defines.GAME_OVER_ENDGAME_PREFIX):
+                            //Parse id and get game info from dictionary.
                             Guid.TryParse(s.Substring(Defines.GAME_OVER_ENDGAME_PREFIX.Length), out gameIDToEnd);
                             Program.clients.TryGetValue(gameIDToEnd, out gameEnded);
+                            //Log end of game and remove client from dictionary.
                             Logger.LogMessage($"Game ended: {gameEnded.CurrentGameFile}, on Client ID: {gameIDToEnd}");
                             Program.clients.Remove(gameIDToEnd);
 
                             break;
+                        //Handle game won case.
                         case string t when t.StartsWith(Defines.GAME_OVER_WON_PREFIX):
+                            //Parse id and get game info from dictionary.
                             Guid.TryParse(s.Substring(Defines.GAME_OVER_WON_PREFIX.Length), out gameIDToEnd);
                             Program.clients.TryGetValue(gameIDToEnd, out gameEnded);
+                            //Log game won.
                             Logger.LogMessage($"Game won: {gameEnded.CurrentGameFile}, on Client ID: {gameIDToEnd}");
                             
                             break;
@@ -119,6 +149,7 @@ namespace A02_TCPIP {
                     break;
             }
             
+            // Log and send response to client.
             Console.WriteLine("Sent: " + response);
             serverBytes = Encoding.ASCII.GetBytes(response);
             stream.Write(serverBytes, 0, serverBytes.Length);
